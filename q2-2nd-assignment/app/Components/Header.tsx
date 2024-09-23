9dfd51bd53c28af7c8209606e58abcdf2bfa9ee2
Adnan ali <153611386+adnanalisamo@users.noreply.github.com>
2024-09-23 11:54:25 +0500

    update file

---
+++ q2-2nd-assignment/app/Components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header>
+        <h1>Wellcome to Next.js</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#"></a>About</li>
+            </ul>
+        </nav>
+        </header>
+    )
+};
---
+++ q2-2nd-assignment/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./Components/Header";
+import About from "./Components/About";
+
+export default function Home() {
+  return (
+ <div>
+  <Header/>
+  <About/>
+
+ </div>
+  );
+}
